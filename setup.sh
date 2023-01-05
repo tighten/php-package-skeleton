a0da9bc18b5e3343cbe193b00353419aa757bde5
Tammy Robinson <tammyprodev@gmail.com>
2023-01-05 08:42:02 -0500

    Default the script prompt to yes

--- setup.sh
+++ setup.sh
@@ -42,10 +42,10 @@ echo    "----------------------------------------------------------------------"
 
 echo
 echo "This script will replace the above values in all files in the project."
-read -p "Are you sure you wish to continue? (n/y) " -n 1 -r
+read -p "Are you sure you wish to continue? (Y/n) " -n 1 -r
 
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ $REPLY =~ ^[Nn]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
